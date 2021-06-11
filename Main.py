6b8ff40205b01eec1c551844bf80fc9a8529d235
seungaaa <73357741+seungaaa@users.noreply.github.com>
2021-06-11 14:06:44 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,15 +4,6 @@ import math
 from World import World
 import turtle
 
-def Debug_Mode():
-    while True:
-        response = input("Run display mode? (y/n)\n")
-        if response.lower() == "y" or response.lower() == "yes":
-            return True
-        elif response.lower() == "n" or response.lower() == "no":
-            return False
-        else:
-            print("Invalid Input: Response must be yes or no. (y/n)")
 def Rerun_Random_World():
     while True:
         print("---------------")
